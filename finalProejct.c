0c9b99f12593dd83196cbe5121ee3baf59d1aed2
Shanti42 <smp163@case.edu>
2018-04-17 19:31:11 -0400

    menu

--- finalProejct.c
+++ finalProejct.c
@@ -8,59 +8,43 @@
 
 
 double mean(double[] data);
-double[] quicksort(double[] data);
-double median(double[] data); //median
+double[] sort(double[] data);
+double[] minMedMax(double[] data); //returns array of min, median, and max
 double quartile(double[] data); //returns
 double sd(double[] data);
-double mode(double[] data);
 
 int main (){
     /*
      PLAN: by default, will return everything, if give input, then will only do some things
      */
-	printf("Please choose what operations you wish to occur ");
+	printf("Choose one or more of the following operations on the data by\n"
+		"typing the coresponding number(s) seperated by a space"
+		"1) Mean \n2)Median \n3)Mode \n4)Standard Deviation \n5)Maximum Value"
+		"\n6)Minimum Value \n7)sorted list \n8)First Quartile \n9)Third Quartile")
 	gets(line);
 	args[0] = strtok(line, " ");
 	while (args[i] != NULL){
 		args[++i] = strtok(NULL, " ");
 	}
+	operations [9] = {0, 0, 0, 0, 0, 0, 0, 0, 0}
+	
 	
 }
 
-//returns the mean
 double mean(double[] data) {
-    int numOfData = data.sizeof;
-    int sum = 0;
-    int i;
-    for(i = 0; i < numOfData; i++) {
-        sum += data[i];
-    }
-    return sum / numOfData;
+    return 0;
 }
 
-//quicksort method
-double[] quicksort(double[] data) {
-    double sorted[data.sizeof];
-    
-    
-    return sorted;
+double[] sort(double[] data) {
+    return data;
 }
 
-//assumes data is sorted
-double median(double[] data) {
-    if(data.sizeof % 2 == 1) { //data size is odd
-        return data[(data.sizeof - 1) / 2];
-    }
-    double val1 = data[data.sizeof / 2]; //first middle value
-    double val2 = data[(data.sizeof - 2) / 2]; //second middle value
-    
-    return (val1 + val2) / 2;
+double[] minMedMax(double[] data) { //returns array of min, median, and max
+    return data;
 }
 
-//assumes data is sorted
-//returns values that make quartiles
 double quartile(double[] data) {
-    
+    return 0;
 }
 
 double sd(double[] data) {
