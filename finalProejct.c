7cca396f9e4a2672e035d558c5044badeaa83b6f
galianda <32524757+galianda@users.noreply.github.com>
2018-04-17 18:50:30 -0400

    added documents

---
+++ finalProejct.c
@@ -0,0 +1,24 @@
+// Caitlin Campbell and Shanti Polara Final Project
+//Beta version
+# include <stdlib.h>
+# include <stdio.h>
+# include <math.h>
+# include <omp.h>
+
+double mean(double[] data);
+double 
+
+int main (){
+	
+}
+
+double shubert(double step, double x2) {
+	double sum1 = 0; 
+	double sum2 = 0;
+	int i;
+	for (i = 1; i <= 5; i++) {
+		sum1 += i * cos((i + 1) * step + i);
+		sum2 += i * cos((i + 1) * x2 + i);
+	}
+	return sum1 * sum2;
+}
