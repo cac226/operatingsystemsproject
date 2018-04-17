e3d937785a9cfa4a50e4337b25440e5d0e17d003
galianda <32524757+galianda@users.noreply.github.com>
2018-04-17 19:04:50 -0400

    updated methods

--- finalProejct.c
+++ finalProejct.c
@@ -6,20 +6,34 @@
 # include <omp.h>
 
 double mean(double[] data);
-double median(double[] data);
+double[] sort(double[] data);
+double[] minMedMax(double[] data); //returns array of min, median, and max
+double quartile(double[] data); //returns
 double sd(double[] data);
 
 int main (){
+    /*
+     PLAN: by default, will return everything, if give input, then will only do some things
+     */
 	
 }
 
-double shubert(double step, double x2) {
-	double sum1 = 0; 
-	double sum2 = 0;
-	int i;
-	for (i = 1; i <= 5; i++) {
-		sum1 += i * cos((i + 1) * step + i);
-		sum2 += i * cos((i + 1) * x2 + i);
-	}
-	return sum1 * sum2;
+double mean(double[] data) {
+    return 0;
+}
+
+double[] sort(double[] data) {
+    return data;
+}
+
+double[] minMedMax(double[] data) { //returns array of min, median, and max
+    return data;
+}
+
+double quartile(double[] data) {
+    return 0;
+}
+
+double sd(double[] data) {
+    return 0;
 }
