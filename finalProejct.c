f538a3cccf09dface0aaede14316c9127447a707
Shanti42 <smp163@case.edu>
2018-04-17 20:11:02 -0400

    operation assign

--- finalProejct.c
+++ finalProejct.c
@@ -21,9 +21,12 @@ int main (){
 	char line[50];
 	char *args[30];
 	int i = 0;
+	int ops[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	int badinput = 0;
+	int holder = 0;
 	printf("Choose one or more of the following operations on the data by\n"
 			"typing the coresponding number(s) seperated by a space"
-			"1) Mean \n2)Median \n3)Mode \n4)Standard Deviation \n5)Maximum Value"
+			"0)all options \n1) Mean \n2)Median \n3)Mode \n4)Standard Deviation \n5)Maximum Value"
 			"\n6)Minimum Value \n7)sorted list \n8)First Quartile \n9)Third Quartile")
 	gets(line);
 	args[0] = strtok(line, " ");
@@ -31,41 +34,12 @@ int main (){
 	while (args[i] != NULL){
 		args[++i] = strtok(NULL, " ");
 	}
-	int badinput = 0;
-	int holder = 0;
-	for (i = 0; i < sizeof(args); i++){
-		if(isdigit(args[i])){
+
+	for (i = 0; i < sizeof(args); i++){ 
+		if(isdigit(args[i])){ //checks if input valid
 			holder = args[i]
-			switch(holder){
-				case 1:
-				
-				break;
-				case 2:
-				
-				break;
-				case 3:
-				
-				break;
-				case 4:
-				
-				break;
-				case 5:
-				
-				break;
-				case 6:
-				
-				break;
-				case 7:
-				
-				break;
-				case 8:
-				
-				break;
-				case 9:
-				
-				break;
-			}
-		
+			if (holder <= 9 && holder >= 0) //lets program know what operations are desired
+				ops[holder] = 1; // uses 1 for the boolean value of true
 		}
 		else
 			badinput = 1;
@@ -74,7 +48,7 @@ int main (){
 		printf("Error: Bad input")
 		return -1;
 	}
-	int operations [9] = {0, 0, 0, 0, 0, 0, 0, 0, 0}
+	
 	
 	
 	
