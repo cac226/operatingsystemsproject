7ff396e5db4880590c096ebc32a0948e05dee28b
Shanti42 <smp163@case.edu>
2018-04-19 22:33:56 -0400

    this is wrong but i wanted to save it

--- finalProject.c
+++ finalProject.c
@@ -13,13 +13,16 @@ one by one, they should have seperate variables they modify*/
 double mean(double data[]);
 double * sort(double data[]);
 double * minMedMax(double data[]); //returns array of min, median, and max
-double quartile(double data[]); //returns
+double * quartile(double data[]); //returns
 double sd(double data[]);
 
 int main (){
     /*
      PLAN: by default, will return everything, if give input, then will only do some things
      */
+	 //data from file must be stored into array called data 
+	double data[10]; /* this is a temperary file for the data that will be given
+	 the values from the input file*/
 	char line[50];
 	char *args[30];
 	int i = 0;
@@ -27,25 +30,31 @@ int main (){
 	int badinput = 0;
 	int holder = 0;
 	int threadcount = 0;
+	//defines data that methods will return
+	double mean, sd;
+	double sort[(sizeof(data)/sizeof(data[0]))], minMedmax[3], quartile[2]; 
 	printf("Choose one or more of the following operations on the data by\n"
 			"typing the coresponding number(s) seperated by a space\n"
 			"0)all options \n1)Mean \n2)Median \n3)Mode \n4)Standard Deviation \n5)Maximum Value"
 			"\n6)Minimum Value \n7)Sorted List \n8)First Quartile \n9)Third Quartile\n");
 	scanf("%d", &line);
 	args[0] = strtok(line, " ");
-	while (args[i] != NULL){
-		args[++i] = strtok(NULL, " ");
-		if(isdigit(args[i])){ //checks if input valid
+	while (args[i] != NULL && strcmp(args[i], "null") != 0){
+		args[i] = strtok(NULL, " ");
+		printf("%s ?\n", args[i]);
+		if(args[i] != NULL && isdigit(args[i])){ //checks if input valid
 			holder = args[i];
 			if (holder <= 9 && holder >= 0) //lets program know what operations are desired
 				ops[holder] = 1; // uses 1 for the boolean value of true
 		}
-		else
+		else{
 			badinput = 1;
+		}
+		i++;
 	}
 	printf("here1\n");
 	if (badinput){
-		printf("Error: Bad input");
+		printf("Error: Bad input\n");
 		return -1;
 	}
 	if (ops[0]){
@@ -140,7 +149,7 @@ double * minMedMax(double data[]) { //returns array of min, median, and max
 
 //assumes data is sorted
 //returns values that make quartiles
-double quartile(double data[]) {
+double * quartile(double data[]) {
     
     return 0;
 }
