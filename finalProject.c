9be5f692e020a361424af7ebb8087b77802a2fb8
galianda <32524757+galianda@users.noreply.github.com>
2018-04-21 20:41:58 -0400

    Revert "created result print statements and thread initilizations"
    This reverts commit 2047082548554a3615826a791c6388a6e5451101.

--- finalProject.c
+++ finalProject.c
@@ -17,14 +17,12 @@ double * minMedMax(double data[]); //returns array of min, median, and max
 double * quartile(double data[]); //returns
 double sd(double data[]);
 
-double minMedmax[3], quartile[2]; 
-
 int main (){
     /*
      PLAN: by default, will return everything, if give input, then will only do some things
      */
 	 //data from file must be stored into array called data 
-	double data[10] = {1,3,4,55,6,21,4,5,7,10} /* this is a temperary file for the data that will be given
+	double data[10]; /* this is a temperary file for the data that will be given
 	 the values from the input file*/
 	char line[20];
 	char *token;
@@ -33,10 +31,11 @@ int main (){
 	int badinput = 0;
 	int threadcount = 0;
 	int con;
+	//printf("here1\n");
 	fflush(stdout);
-	double sort[(sizeof(data)/sizeof(data[0]))];
 	//defines data that methods will return
 	double mean, sd;
+	//double sort[(sizeof(data)/sizeof(data[0]))], minMedmax[3], quartile[2]; 
 	printf("Choose one or more of the following operations on the data by\n"
 			"typing the coresponding number(s) seperated by a space\n"
 			"0)all options \n1)Mean \n2)Median \n3)Mode \n4)Standard Deviation \n5)Maximum Value"
@@ -44,7 +43,6 @@ int main (){
 	fgets(line, 20, stdin);
 	printf("%s-line", line);
 	token = strtok(line, " ");
-	//reading in string, making sure only numbers were entered
 	while(token != NULL ) {
 		if (isdigit(*token)){
 			con = *token - '0';
@@ -56,37 +54,31 @@ int main (){
 			badinput = 1;
 		token = strtok(NULL, " ");
 	}
+   
 	
-	//initialize thread ids, can have up to 6 threads, so prepair 6 tids
-	pthread_t tid[6]; 
-	
-	//error message if non-numbers were entered
+	printf("here1\n");
+	fflush(stdout);
 	if (badinput){
 		printf("Error: Bad input\n");
 		return -1;
 	}
-	
-	//if user requests all operations, set all operations to true
 	if (ops[0]){
 		for (i = 1; i < sizeof(ops)/sizeof(ops[0]); i++)
 			ops[i] = 1;
 	}
-	
-	//begin thread creation, keeping track of number of threads
+	printf("here3\n");
 	if (ops[1]){ //mean
-		pthread_create(&tid[1], mean, mean, data);
-		threadcount++;
+		printf("mean\n");
 	}
 	
 	if ((ops[2] || ops[5]) || ops[6]){ //median, max, min
+		
 		if (ops[2])//median
 			printf("median\n");
 		if (ops[6]) //minumum
 			printf("minimum\n");
 		if (ops[5])//maximum
 			printf("maximum\n");	
-		//pthread_create(&tid[2], NULL, minmedmax, data);
-		//threadcount++;
 	}
 	
 	if (ops[3]){ //mode
@@ -99,7 +91,7 @@ int main (){
 	
 	if (ops[7]){ //sorted list
 		double list[3] = {1.0, 2.0, 3.0};
-		for (i = 0; i < sizeof(list)/sizeof(list[0]); i++){//replace with data set when sort method ready
+		for (i = 0; i < sizeof(list); i++){//replace with data set when sort method ready
 			printf("%.2f ", list[i]);
 		}
 	}
@@ -113,27 +105,7 @@ int main (){
 			printf("second quartile\n");
 		}
 	}
-	
-	for (i = 0; i < threadcount; i++)
-		pthread_join(tid[i], NULL);
-	
-	//print statements for results
-	if(ops[1])
-		printf("Mean = %.2f\n", mean);
-	if(ops[2])
-		printf("Median = %.2f\n", minMedmax[1]);
-	if(ops[3])
-		printf("Mode = %.2f\n", mode);
-	if(ops[4])
-		printf("Standard Deviation = %.2f\n", sd);
-	if(ops[5])
-		printf("Maximum Value = %.2f\n", minMedmax[2]);
-	if(ops[6])
-		printf("Minimum Value = %.2f\n", minMedmax[0]);
-	if(ops[8])
-		printf("First Quartile = %.2f\n", quartile[0]);
-	if(ops[9])
-		printf("Third Quartile = %.2f\n", quartile[1]);
+		
 	return 0;
 	
 	
@@ -149,6 +121,7 @@ double mean(double data[]) {
         sum += data[i];
     }
     return sum / numOfData;
+    return 0;
 }
 
 //quicksort method
@@ -175,7 +148,6 @@ double median(double data[]) {
     return (val1 + val2) / 2;
 }
 
-//indexes:min = 0, med = 1, med =2
 double * minMedMax(double data[]) { //returns array of min, median, and max
     return data;
 }
